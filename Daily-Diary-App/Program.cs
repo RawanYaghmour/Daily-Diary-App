21fcbf0ffff0c90bb37b4917a79e176bebe50fc2
RawanYaghmour <Rawan.yag02@gmail.com>
2024-06-28 20:50:43 +0300

    Add project files.

---
+++ Daily-Diary-App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Daily_Diary_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
